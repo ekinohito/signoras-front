5e6509edc9470fd54b8b76495bbf5ca6b6d74bf1
ekinohito <zhenyagur9@gmail.com>
2021-09-17 22:15:40 +0300

    mobile

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {AppBar, Button, Container, IconButton, Link, Toolbar, Typography} from "@mui/material";
+import {AppBar, Box, Button, Container, IconButton, Link, Toolbar, Typography} from "@mui/material";
 import {Menu} from "@mui/icons-material";
 import logo from "../public/assets/logo.png"
 import Image from "next/image"
@@ -12,8 +12,8 @@ export default function Header() {
                 <Typography variant="h4" component="div" sx={{flexGrow: 1, textTransform: "uppercase", fontWeight: "700"}}>
                     Signoras
                 </Typography>
-                <NextLink href="/" passHref><Link sx={{mx: 2, textDecoration: "none"}} variant="h6" color="secondary">Главная</Link></NextLink>
-                <NextLink href="/eco" passHref><Link sx={{mr: 2, textDecoration: "none"}} variant="h6" color="secondary">Экология</Link></NextLink>
+                <NextLink href="/" passHref><Link sx={{mx: 2, textDecoration: "none", display: {xs: "none", sm: "unset"}}} variant="h6" color="secondary">Главная</Link></NextLink>
+                <NextLink href="/eco" passHref><Link sx={{mr: 2, textDecoration: "none", display: {xs: "none", sm: "unset"}}} variant="h6" color="secondary">Экология</Link></NextLink>
                 <Image src={logo} alt="logo" width="50" height="50"/>
             </Toolbar>
         </Container>
