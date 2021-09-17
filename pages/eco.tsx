39cf980b19fff39bf98ab4b0eab5c9fa8a3b3e98
ekinohito <zhenyagur9@gmail.com>
2021-09-17 23:13:13 +0300

    final

--- pages/eco.tsx
+++ pages/eco.tsx
@@ -2,7 +2,18 @@ import {NextPage} from "next";
 import Head from "next/head";
 import React, {useState} from "react";
 import Header from "../src/Header";
-import {Box, Checkbox, Container, FormControlLabel, FormGroup, Radio, RadioGroup, Tab, Tabs} from "@mui/material";
+import {
+    Box,
+    Checkbox,
+    Container,
+    FormControlLabel,
+    FormGroup,
+    Radio,
+    RadioGroup,
+    Tab,
+    Tabs,
+    Typography
+} from "@mui/material";
 import {Check, ecoCars, ecoHouses} from "../data/eco";
 
 const Eco: NextPage = () => {
@@ -27,26 +38,34 @@ const Eco: NextPage = () => {
             <FormGroup>
             {({
                 house: <>
+                    <Typography variant="h4" sx={{my: 3}}>Выберите соответствующие параметры </Typography>
                     {ecoHouses.map(value => <FormControlLabel
                     key={value.label}
                     control={<Checkbox
                         onChange={(event, checked) => setHouseValue({...houseValue, [value.label]: checked})} checked={houseValue[value.label]}/>}
                     label={value.label} />)}
-                    {ecoHouses.map(value => houseValue[value.label]?value.percentage:0).reduce((previousValue, currentValue) => previousValue + currentValue)}%
+
+                    <Typography color="primary" variant="h3">
+                        {ecoHouses.map(value => houseValue[value.label]?value.percentage * 0.05:0).reduce((previousValue, currentValue) => previousValue + currentValue).toFixed(2)}%
+                    </Typography>
                 </>,
                 car: <>
+                    <Typography variant="h4" sx={{my: 3}}>Выберите тип двигателя</Typography>
                     <RadioGroup
                         value={carValue}
                         onChange={((event, value) => setCarValue(value))}>
                         <FormControlLabel
                             control={<Radio value=""/>}
-                            label="Нет" />
+                            label="Другой" />
                         {ecoCars.map(value => <FormControlLabel
                             key={value.label}
                             control={<Radio value={value.label}/>}
                             label={value.label} />)}
                     </RadioGroup>
-                    {ecoCars.find(value => value.label === carValue)?.percentage || 0}%
+                    <Typography color="primary" variant="h3">
+                        Скидка: {((ecoCars.find(value => value.label === carValue)?.percentage || 0)* 0.05).toFixed(2)}%
+                    </Typography>
+
                 </>
             })[tab]}
             </FormGroup>
